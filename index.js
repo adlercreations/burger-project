4b37642f17168dac0a49082b7d375a563106df50
Stephen Adler <adlercreations@gmail.com>
2024-07-24 13:32:46 -0400

    added more stuff

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const myName = prompt("Whats your name?")
+const burgersList = document.getElementById(`burgers-list`)
 
-console.log(myName)
+fetch("http://localhost:4000/burgers")
+.then(response => response.json())
+.then(burgers => {
+    burgers.forEach(addBurgerToBurgersList)
+})
+    
+function addBurgerToBurgersList(burger) {
+    const liElement = documetn.createElement(`li`)
+
+    const idElement = document.createElement(`h3`)
+    idElement.textContent = `Burger # ${burger.id}`
+    
+    const nameElement = document.createElement(`h3`)
+    nameElement.textContent = `Name: ${burger.name}`
+
+    const imageELement = document.createElement(`img`)
+    imageELement.src = burger.image
+
+    
+
+    liElement.appendChild(nameElement)
+    burgersList.appendChild(liElement)
+}
